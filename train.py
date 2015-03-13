23f777ef59e5af8a61d5b3ba8915823aebb13a67
Shawn Tan <shawn@wtf.sg>
2015-03-13 16:43:04 +0800

    First successful model.

--- train.py
+++ train.py
@@ -1,15 +1,19 @@
+import sys
 import theano
 import theano.tensor as T
 import numpy as np
-import lstm
 import cPickle as pickle
-from theano_toolkit import utils as U
-from theano_toolkit.parameters import Parameters
-from theano_toolkit import updates
+from itertools import islice
+
 import model
 import vocab
 import data_io
-import sys
+import lstm
+
+
+from theano_toolkit import utils as U
+from theano_toolkit.parameters import Parameters
+from theano_toolkit import updates
 def make_functions(inputs,outputs,params,grads):
 	acc_grads = [ theano.shared(np.zeros(p.get_value().shape,dtype=np.float32)) for p in params ]
 	count = theano.shared(np.float32(0))
@@ -70,16 +74,32 @@ if __name__ == "__main__":
 
 	print "Compiling native...",
 	acc,update = make_functions(inputs,outputs,params,grads)
+	test = theano.function(
+			inputs = [story,idxs,qstn,ans_evd,ans_lbl],
+			outputs = [
+				T.neq(T.argmax(output_evd),ans_evd),
+				T.neq(T.argmax(output_ans),ans_lbl),
+			]
+		)
+
+
 	print "Done."
 	batch_size = 10 
+
+	instance_count = sum(1 for _ in data_io.group_answers(training_file))
+	train_instance_count = int(0.9 * instance_count)
+	print "Total:",instance_count,"Training:",train_instance_count
+	best_error = np.inf
 	for epoch in xrange(30):
 		group_answers = data_io.group_answers(training_file)
-		training_data = data_io.story_question_answer_idx(group_answers,vocab_in,vocab_out)
+		train_group_answers = islice(group_answers,train_instance_count)
+		training_data = data_io.story_question_answer_idx(train_group_answers,vocab_in,vocab_out)
 		training_data = data_io.randomise(training_data)
 		
 		loss  = 0
 		count = 0
 		for input_data,idxs,question_data,ans_w,ans_evd in training_data:
+			pass
 			loss  += acc(input_data,idxs,question_data,ans_evd,ans_w)
 			count += 1
 			if count == batch_size:
@@ -87,5 +107,14 @@ if __name__ == "__main__":
 				print loss/count
 				loss  = 0
 				count = 0
-
 		update()
+
+		test_data = data_io.story_question_answer_idx(group_answers,vocab_in,vocab_out)
+		errors = sum(
+				np.array(test(input_data,idxs,question_data,ans_evd,ans_w),dtype=np.float32)
+				for input_data,idxs,question_data,ans_w,ans_evd in test_data
+			 )/(instance_count - train_instance_count)
+		print errors
+		if errors.sum() < best_error:
+			P.save('model.pkl')
+			best_error = errors.sum()
