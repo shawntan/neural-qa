75a4e1bec09ac535256b9879743cfe6bd6d54915
Shawn Tan <shawn@wtf.sg>
2015-08-31 22:49:57 +0800

    Modify model testing

--- test_model.py
+++ test_model.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
         )
 
     story = T.imatrix('story')
-    output_ans,output_evds = attention(story)
+    output_ans,output_evds = attention(story,test=True)
     answer = theano.function(
             inputs = [story],
             outputs = output_evds+[output_ans]
@@ -37,36 +37,40 @@ if __name__ == "__main__":
     for key,val in vocab_in.iteritems(): rev_map[val] = key
 
     rev_map[-1] = "<unk>"
-    import random
-    
-    for _ in xrange(3): training_set.next()
-    input_data,ans_evds,ans_w = training_set.next()
-    sentences = [ ' '.join(rev_map[input_data[i,j]] 
-                    for j in xrange(input_data.shape[1]))
-                        for i in xrange(input_data.shape[0]) ]
-    pprint(sentences)
-    evidence_answer = answer(input_data)
-    evd_prob = evidence_answer[:evidence_count]
-    ans_prob = evidence_answer[-1]
-    
-    print "Evidences:"
-    for i,e in enumerate(evd_prob): 
-        print e
+    total = 0
+    right = 0
+    for input_data,ans_evds,ans_w in training_set: 
+        sentences = [ ' '.join(rev_map[input_data[i,j]] 
+                        for j in xrange(input_data.shape[1]))
+                            for i in xrange(input_data.shape[0]) ]
+        pprint(sentences)
+        evidence_answer = answer(input_data)
+        evd_prob = evidence_answer[:evidence_count]
+        ans_prob = evidence_answer[-1]
+        print "Evidences:"
+        for i,e in enumerate(evd_prob): 
+            print e
+            print "predicted",
+            hinton.plot(e,max_arr=1)
+            correct = np.zeros((e.shape[0],))
+            correct[ans_evds[i]] = 1
+            print "correct  ",
+            hinton.plot(correct)
+
+        print "Answer:"
         print "predicted",
-        hinton.plot(e,max_arr=1)
-        correct = np.zeros((e.shape[0],))
-        correct[ans_evds[i]] = 1
+        hinton.plot(ans_prob,max_arr=1)
+
+        correct = np.zeros((ans_prob.shape[0],))
+        correct[ans_w] = 1
         print "correct  ",
-        hinton.plot(correct)
+        hinton.plot(correct,max_arr=1)
 
-    print "Answer:"
-    print "predicted",
-    hinton.plot(ans_prob,max_arr=1)
+        right += (np.argmax(ans_prob) == ans_w)
+        total += 1
 
-    correct = np.zeros((ans_prob.shape[0],))
-    correct[ans_w] = 1
-    print "correct  ",
-    hinton.plot(correct,max_arr=1)
+    print right / float(total)
+
 
 
 
